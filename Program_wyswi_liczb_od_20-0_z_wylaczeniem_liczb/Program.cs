1f96bc7e58dc53eedbc91e43cd9424a5064e9d56
Daniel_Kaznowski <Daniel_Kaznowski@MR_ROBOT>
2024-11-12 23:58:49 +0100

    Dodaj pliki projektów.

---
+++ Program_wyswi_liczb_od_20-0_z_wylaczeniem_liczb/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 20; i >= 0; i--)
+        {
+            // Sprawdzenie, czy liczba znajduje się na liście wykluczonych
+            if (i == 2 || i == 6 || i == 9 || i == 15 || i == 19)
+            {
+                continue; // Pominięcie liczby
+            }
+            Console.WriteLine(i);
+        }
+    }
+}
